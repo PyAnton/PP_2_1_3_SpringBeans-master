3c425134b4d5b99a0a63fb908b505b1ab5f82c08
Antonmak112 <pyanton112@gmail.com>
2023-07-20 21:06:01 +0500

    yes

---
+++ src/main/java/app/config/AppConfig.java
@@ -0,0 +1,21 @@
+package app.config;
+
+import app.model.Cat;
+import app.model.Dog;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "app")
+public class AppConfig {
+    @Bean
+    public Cat cat() {
+        return new Cat();
+    }
+
+    @Bean
+    public Dog dog() {
+        return new Dog();
+    }
+}
